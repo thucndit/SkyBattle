1c70a9569f2295050c75ad42cb5a251771871fec
ThucND <thucnd.it@gmail.com>
2017-09-24 15:28:50 +0700

    Finish

--- app/src/main/java/utc/it/thucnd/skybattle/GamePanel.java
+++ app/src/main/java/utc/it/thucnd/skybattle/GamePanel.java
@@ -26,7 +26,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     public static final int HEIGHT = 768;
     public static float scaleX;
     public static float scaleY;
-    public static final int MOVESPEED = 6;
+    public static final int MOVESPEED = 5;
 
     private Context gameContext;
     private int randmap;
